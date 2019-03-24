8d4bf83431049656bbf308f78f61361379877d82
Islam Gaber <45578528+islamgab@users.noreply.github.com>
2019-03-24 21:16:32 +0200

    Update Q5.sql

--- Q5.sql
+++ Q5.sql
@@ -1,4 +1,4 @@
-WITH t1 AS 
+WITH tab1 AS 
 (
 	SELECT c.CustomerId, c.FirstName, c.LastName, c.Country, SUM(i.Total) TotalSpent
 	FROM 
@@ -7,13 +7,13 @@ WITH t1 AS
 	GROUP BY c.CustomerId
 )
 
-SELECT t1.*
-FROM t1
+SELECT tab1.*
+FROM tab1
 JOIN(
 	SELECT CustomerId, FirstName, LastName, Country, MAX(TotalSpent) AS TotalSpent
-	FROM t1
+	FROM tab1
 	GROUP BY Country
-)t2
-ON t1.Country = t2.Country
-WHERE t1.TotalSpent = t2.TotalSpent
+)tab2
+ON tab1.Country = tab2.Country
+WHERE tab1.TotalSpent = tab2.TotalSpent
 ORDER BY Country;
